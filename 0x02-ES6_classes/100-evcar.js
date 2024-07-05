23dca1039bce75b964961ddefaaf4952990bf8a4
kiddingmz <bata.isidro@gmail.com>
2024-07-05 10:21:25 +0200

    CHORE:  11. EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
+
+	get range() {
+		return this._range;
+	}
+
+	static get [Symbol.species]() {
+		return Car;
+	}
+}
