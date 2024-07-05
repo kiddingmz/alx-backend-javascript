1fb970524dc33abe218319f2d16db879a1cfb4f6
kiddingmz <bata.isidro@gmail.com>
2024-07-05 09:12:46 +0200

    CHORE: improves the indent

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((resolve, reject) => {
+  return new Promise((reject) => {
     reject(new Error(`${filename} cannot be processed`));
   });
 }
