d5e05b5a1318fb924f265d9b6d77ed6e6ef3d79a
kiddingmz <bata.isidro@gmail.com>
2024-07-05 08:31:14 +0200

    FEAT: 3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 /* eslint-disable comma-dangle */
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-    return Promise.all([
-        uploadPhoto(),
-        createUser()
-    ]).then((values) => {
-        console.log(values[0].body, values[1].firstName, values[1].lastName);
-    }).catch(() => console.log('Signup system offline'));
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((values) => {
+            console.log(
+                `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
+            );
+        })
+        .catch(() => console.log('Signup system offline'));
 }
